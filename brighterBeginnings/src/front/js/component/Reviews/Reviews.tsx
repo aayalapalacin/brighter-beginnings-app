4cc16ca7ed4e5d469fa41465eb9bc29ed3ae456c
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-05-20 01:04:48 +0000

    added titles and subtitles to the banner of the reviews image banner

--- brighterBeginnings/src/front/js/component/Reviews/Reviews.tsx
+++ brighterBeginnings/src/front/js/component/Reviews/Reviews.tsx
@@ -12,12 +12,16 @@ interface ReviewsProps {
 const Reviews: React.FC<ReviewsProps> = ({ reviewLinks }) => {
   return (
     <div className="reviews-container w-100 mx-auto">
-      <div className="row reviews-page-img-container w-100">
+      <div className="row reviews-page-img-container w-100 position-relative">
         <img src="/reviews_images/ceo.jpeg" alt="Banner" className="w-100 reviews-page-img" />
+        <div className="img-text-overlay">
+          <h1 className="img-title">Lily Smith</h1>
+          <p className="img-subtitle">Brighter Beginnings Director</p>
+        </div>
       </div>
       <div className="row reviews-paragraph-container">
         <div className="col reviews-paragraphs">
-          <p>We know placing your child in someone else care is a big and hard decision. That's why it's important to us that you have an honest and unbiased idea of what our services are like for other families.</p>
+          <p>We know placing your child in someone else's care is a big and hard decision. That's why it's important to us that you have an honest and unbiased idea of what our services are like for other families.</p>
           <p>Please carefully review our public reviews that we do not manage in any way so you can make the best decision for your family.</p>
           <p>We believe our program has a lot to offer and hope it aligns with you and your little ones' best interest.</p>
           <p> -Brighter Beginnings </p>
