71e7110f0db3a7ca91396616c874dd4ddc8ae669
avokeys <alexander.ayala.palacin@gmail.com>
2024-05-23 02:28:01 +0000

    changed reviews structure, updated styles, made progress responsiveness

--- brighterBeginnings/src/front/js/layout.tsx
+++ brighterBeginnings/src/front/js/layout.tsx
@@ -29,16 +29,7 @@ const Layout = () => {
           <Route element={<Staff />} path="/staff" />
           {/* <Route element={<Contact />} path="/contact" /> */}
           {/* route with props for reviews */}
-          <Route 
-            element={<Reviews 
-                        reviewLinks={{
-                          google: "https://www.google.com/search?q=your+business+reviews",
-                          facebook: "https://www.facebook.com/yourbusiness/reviews",
-                          yelp: "https://www.yelp.com/biz/yourbusiness"
-                        }}
-                      />} 
-            path="/reviews" 
-          />
+          <Route element={<Reviews />} path="/reviews" />
           <Route element={<Photos />} path="/photos" />
           <Route element={<Careers />} path="/careers" />
         </Routes>
--- brighterBeginnings/src/front/js/pages/reviews.tsx
+++ brighterBeginnings/src/front/js/pages/reviews.tsx
@@ -1,18 +1,68 @@
 import React from 'react';
-import Reviews from "../component/Reviews/Reviews"
+import "../../styles/reviews.css";
 
-const App = () => {
+
+const reviewData :{imgSrc:string ;link: string; alt:string;}[] =
+[
+    {
+      imgSrc: "/reviews_images/facebook.png",
+      link: "https://www.facebook.com/Brighter.Beginnings.Child.Care/reviews",
+      alt:"facebook_img" 
+    },
+    {
+      imgSrc: "/reviews_images/google.webp",
+      link: "https://www.google.com/search?q=brighter+beginnings&oq=brighter+beginnings&gs_lcrp=EgZjaHJvbWUqDAgAECMYJxiABBiKBTIMCAAQIxgnGIAEGIoFMhYIARAuGIMBGK8BGMcBGLEDGIAEGI4FMgYIAhBFGEAyBwgDEAAYgAQyBwgEEAAYgAQyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDyoAgCwAgA&sourceid=chrome&ie=UTF-8#lrd=0x89e6db75dd2dd64d:0xecbc9c8277fd7644,1,,,,", 
+      alt:"google_img"
+    },
+    {
+      imgSrc: "/reviews_images/yelp.webp",
+      link: "https://www.yelp.com/biz/brighter-beginnings-preschool-child-care-south-hadley#reviews", 
+      alt:"yelp_img"
+    },
+    {
+      imgSrc: "/reviews_images/care.png",
+      link: "https://www.care.com/b/l/brighter-beginnings-child-care-llc/south-hadley-ma", 
+      alt:"care_img"
+    },
+]
+const Reviews = () => {
   return (
-    <Reviews 
-      reviewLinks={{
-        google: "https://www.google.com/search?q=your+business+reviews",
-        facebook: "https://www.facebook.com/yourbusiness/reviews",
-        yelp: "https://www.yelp.com/biz/yourbusiness"
-        // care: "https://www.care.com/biz/yourbusiness"
-      }}
-    />
+    <div className="reviews-container w-100 mx-auto">
+      <div className="row reviews-page-img-container w-100 position-relative">
+        <img src="/reviews_images/ceo.jpeg" alt="Banner" className="w-100 reviews-page-img" />
+        <div className="img-text-overlay">
+          <h1 className="img-text-overlay-title ">Lily Smith</h1>
+          <p className="img-overlay-subtitle mt-1">Brighter Beginnings Director</p>
+        </div>
+      </div>
+      <div className="row reviews-paragraph-container">
+        <div className="col reviews-paragraphs">
+          <p className='reviews-p-1'>We know placing your child in someone else's care is a big and hard decision. That's why it's important to us that you have an honest and unbiased idea of what our services are like for other families.</p>
+          <p className='reviews-p-2 d-none d-md-block'>Please carefully review our public reviews that we do not manage in any way so you can make the best decision for your family.</p>
+          <p className='reviews-p-3 d-none d-md-block'>We believe our program has a lot to offer and hope it aligns with you and your little ones' best interest.</p>
+          <p className='reviews-p-4'> -Brighter Beginnings </p>
+        </div>
+      </div>
+      <div className="row reviews-scroll-container my-5">
+        <div className="col d-flex reviews-scroll">
+          {reviewData.length > 0 ? reviewData.map((reviewContent, reviewContentIndex)=>{
+            return(
+              <div className="review-item-container mx-1">
+                <img src={reviewContent.imgSrc} alt={reviewContent.alt} className="review-item-image mb-3" />
+                <button onClick={() => window.open(reviewContent.link, "_blank")} className="review-item-button">Visit Reviews</button>
+              </div>
+            )
+            
+          })
+        :
+        <div className='no-reviews m-auto'>
+            <h1 className='reviews-title'>Reviews did not load properly</h1>
+        </div>
+        }
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default Reviews;
-
