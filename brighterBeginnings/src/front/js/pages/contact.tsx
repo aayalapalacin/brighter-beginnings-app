def571f99abb1bc1c16fcdd80a22e0a38e3fefa2
avokeys <alexander.ayala.palacin@gmail.com>
2024-06-03 03:29:21 +0000

    updated structure of programs  page

--- brighterBeginnings/src/front/js/pages/contact.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Contact = () => {
-  // NOT SURE IF WE WILL INCLUDE THIS maybe use it in the careers page
-  return <div>contact</div>;
-};
-
-export default Contact;
