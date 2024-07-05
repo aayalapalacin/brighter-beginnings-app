d3b8b0c4b2c639fbf1c142926257368adf092b57
avokeys <alexander.ayala.palacin@gmail.com>
2024-07-05 03:32:37 +0000

    updated responsive styles

--- brighterBeginnings/src/front/js/component/Footer.tsx
+++ brighterBeginnings/src/front/js/component/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     "https://www.google.com/maps/place/Brighter+Beginnings+Child+Care/@42.2325078,-72.5689313,16z/data=!3m1!4b1!4m6!3m5!1s0x89e6db75dd2dd64d:0xecbc9c8277fd7644!8m2!3d42.2325078!4d-72.5663564!16s%2Fg%2F1tgw619r?entry=ttu";
 
   return (
-    <div className="footer-container  w-100 pt-2">
+    <div className="footer-container mb-lg-0 mb-md-0 mb-3  w-100 pt-2">
       <div className="footer-content d-flex text-center container">
         <div className="footer-phone-container d-md-flex d-lg-flex d-block border-3 border-end pe-md-3 pe-lg-3 pe-2">
           <span className="footer-phone fa fa-phone my-auto p-2 " />
